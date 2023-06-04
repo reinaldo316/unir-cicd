96bf8322338781de4e01fc91912f978d9903b878
Edgar guerra <reinaldo316@gmail.com>
2023-06-04 16:06:15 -0500

    cambios

---
+++ jenkinsfile.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent {
+        label 'docker'
+    }
+    stages {
+        stage('Source') {
+            steps {
+                git 'https://github.com/reinaldo316/unir-cicd.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Building stage!'
+                sh 'make build'
+            }
+        }
+        stage('Unit tests') {
+            steps {
+                sh 'make test-unit'
+                archiveArtifacts artifacts: 'results/unit_result.xml'
+            }
+        }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/api_result.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/cypress_result.xml'
+            }
+        }
+    }
+    post {
+        always {
+            junit 'results/*_result.xml'
+            cleanWs()
+        }
+        failure {
+            emailext (
+                to: 'reinaldo316@gmail.com',
+                subject: "Falló el trabajo: \${JOB_NAME} - Build #\${BUILD_NUMBER}",
+                body: "El trabajo \${JOB_NAME} ha fallado en la ejecución número \${BUILD_NUMBER}. Por favor, revisa los detalles.",
+                attachLog: true
+            )
+        }
+    }
+}
